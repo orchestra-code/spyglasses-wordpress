282a461ad625457d033b5b196bebe6d25dde11e2
Jim Wrubel <jwrubel@gmail.com>
2025-07-09 13:38:03 -0400

    chore: remove author URI

--- spyglasses.php
+++ spyglasses.php
@@ -5,7 +5,6 @@
  * Description: Detect, block and log your website's AI traffic.
  * Version: 1.1.6
  * Author: Orchestra AI, Inc.
- * Author URI: https://www.spyglasses.io
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: spyglasses
